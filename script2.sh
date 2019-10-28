712fe606a09a65f746cb026596a818e2ad1cda97
agavrel <gavrel.antonin@gmail.com>
2019-10-28 21:43:13 +0100

    [ADD] another script

--- script2.sh
+++ script2.sh
@@ -6,13 +6,13 @@ read input
 DAY="$(date -d $input +'%s')" # user epoch input
 TODAY="$(date -d 'now' +'%s')" # today's epoch
 
-DIFF="$(( $(($TODAY - $DAY)) / (60*60*24) ))" # https://stackoverflow.com/questions/4946785/how-to-find-the-difference-in-days-between-two-dates
+DIFF="$(( $(($TODAY - $DAY)) / (3600*24) ))" # https://stackoverflow.com/questions/4946785/how-to-find-the-difference-in-days-between-two-dates
 
 echo -e "\e[34mThank you!" # https://misc.flogisoft.com/bash/tip_colors_and_formatting
 
 if (( $DIFF >= 0)); then
-	echo -e "\e[92m${DIFF} elapsed."
+	echo -e "\e[92m${DIFF} days elapsed. ^^"
 else
 	
-	echo -e "\e[92m${DIFF#-} remaining." # https://stackoverflow.com/questions/29223313/absolute-value-of-a-number
+	echo -e "\e[92m${DIFF#-} days remaining. TT" # https://stackoverflow.com/questions/29223313/absolute-value-of-a-number
 fi
